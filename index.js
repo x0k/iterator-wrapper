9f3bb2b9108b44b637334d9674e0da70b8aed624
Krasilnikov Roman <x0kill@ya.ru>
2019-04-08 17:37:18 +0300

    Added other dateTime fields

--- index.js
+++ index.js
@@ -6,36 +6,67 @@ const buildIterable = available => function* (value) {
   return value
 }
 
+const getMonthLength = (year, month) => new Date(year, month + 1, 0).getDate();
+
 const years = buildIterable(() => true)
 const months = buildIterable(({ month }) => month < 12)
+const days = buildIterable(({ year, month, day }) => day < getMonthLength(year, month))
+const hours = buildIterable(({ hour }) => hour < 24)
+const minutes = buildIterable(({ minute }) => minute < 60)
 
-const from ={ year: 2000, month: 10 }
-const to = { year: 2001, month: 2 }
+const from = { year: 2000, month: 11, day: 29, hour: 23, minute: 58 }
+const to = { year: 2001, month: 0, day: 0, hour: 1, minute: 3 }
 
-const wrap = (iterable, wrapper, handler) => function* (value) {
+const wrap = (iterable, handler, wrapper) => function* (value) {
   const iterator = iterable(value)
   let item = iterator.next(handler)
   while (!item.done) {
-    const data = yield * wrapper(item.value)
-    item = iterator.next(() => handler(data))
+    if (wrapper) {
+      const data = yield* wrapper(item.value)
+      item = iterator.next(() => handler(data))
+    } else {
+      yield item.value
+      item = iterator.next(handler)
+    }
   }
   return item.value
 }
 
-const handle = (iterable, next) => function* (value) {
+const handle = (iterable, handler, available) => function* (value) {
   const iterator = iterable(value)
-  let item = iterator.next(next)
-  while (!item.done) {
+  let item = iterator.next(handler)
+  while (!item.done && available(item.value)) {
     yield item.value
-    item = iterator.next(next)
+    item = iterator.next(handler)
   }
   return item.value
 }
 
-const dateTime = handle(
-  wrap(years, months,
-    ({ year, month }) => ({ year: year + Math.floor(month/12), month: month%12 })),
-  ({ month, ...rest }) => ({ month: month + 1, ...rest }))
+const dateTime =
+handle(
+  wrap(
+    wrap(
+      wrap(
+        wrap(
+          years,
+          ({ year, month, ...rest }) => ({ year: year + Math.floor(month / 12), month: month % 12, ...rest }),
+          months
+        ),
+        ({ year, month, day, ...rest }) => {
+          const len = getMonthLength(year, month)
+          return { month: month + Math.floor(day/len), day: day%len, ...rest }
+        },
+        days
+      ),
+      ({ day, hour, ...rest }) => ({ day: day + Math.floor(hour/24), hour: hour%24, ...rest }),
+      hours
+    ),
+    ({ hour, minute, ...rest }) => ({ hour: hour + Math.floor(minute/60), minute: minute%60, ...rest }),
+    minutes
+  ),
+  ({ minute, ...rest }) => ({ minute: minute + 30, ...rest }),
+  ({ year, month, day, hour, minute }) => year < to.year || month < to.month || day < to.day || hour < to.hour || minute <= to.minute
+)
 
 const items = []
 
