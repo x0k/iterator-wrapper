e64461f6bd879ed2bbc4000496b531ec85dc6105
Krasilnikov Roman <x0kill@ya.ru>
2019-04-10 11:37:00 +0300

    Change methods return type

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 export type wrapper<T, D> = (value: T, data: D) => IterableIterator<T>
 
-export function * decorate<T, R> (iterable: IterableIterator<T>, wrapper: wrapper<R, T>, initialValue: R) {
+export type IterableBuilder<T> = (initialValue: T) => IterableIterator<T>
+
+export const decorate = <T, R extends T> (iterable: IterableBuilder<T>, wrapper: wrapper<R, T>): IterableBuilder<R> => function * (initialValue: R) {
   let value = initialValue
-  for (const item of iterable) {
+  for (const item of iterable(initialValue)) {
     value = yield * wrapper(value, item)
   }
   return value
 }
 
-export function * wrap<T> (iterable: IterableIterator<T>, available: (value: T) => boolean) {
-  let item = iterable.next()
+export const wrap = <T> (iterable: IterableBuilder<T>, available: (value: T) => boolean) => function * (value: T) {
+  const iterator = iterable(value)
+  let item = iterator.next()
   while (!item.done && available(item.value)) {
     yield item.value
-    item = iterable.next()
+    item = iterator.next()
   }
   return item.value
 }
